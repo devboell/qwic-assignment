9617d605bc9d1fc368fe171d479fdaae86d40b65
devboell <devboell@gmail.com>
2018-07-05 16:12:10 +0200

    setup store and reducer

---
+++ src/App/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  test: 'test state',
+}
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { createStore } from 'redux'
+
 import App from './App'
+import reducer from './App/reducer'
+
+const store = createStore(reducer)
 
 
 ReactDOM.render(<App />,
