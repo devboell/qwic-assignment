2b3c30cabc827121574ed6f96cd1c8c659dc6446
devboell <devboell@gmail.com>
2018-07-06 14:07:21 +0200

    configure thunk and devTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import reducer from './App/reducer'
 
-const store = createStore(reducer)
+
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk)),
+)
 
 
 ReactDOM.render(
