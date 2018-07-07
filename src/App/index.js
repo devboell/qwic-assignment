092330c407ee11d9d796861bc3c84b741abef5a5
devboell <devboell@gmail.com>
2018-07-07 08:30:04 +0200

    add PropTypes & cleanup

--- src/App/index.js
+++ src/App/index.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import { fetchCharacters } from './thunks'
 
 class App extends Component {
-
   componentDidMount() {
-    this.props.onFetchCharacters()
+    const { onFetchCharacters } = this.props
+    onFetchCharacters()
   }
 
   render() {
     const { characters } = this.props
-    return characters.map(chr =>
-      <h1 key={chr.id}>{chr.id}</h1>
-    )
+    return characters.map(chr => (
+      <h1 key={chr.id}>
+        {chr.id}
+      </h1>))
   }
+}
 
+App.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  onFetchCharacters: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
-  characters: state.characters
+  characters: state.characters,
 })
 
 const mapDispatchToProps = dispatch => ({
-  onFetchCharacters: () => dispatch(fetchCharacters())
+  onFetchCharacters: () => dispatch(fetchCharacters()),
 })
-  
+
 export default connect(mapStateToProps, mapDispatchToProps)(App)
