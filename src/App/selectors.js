244399ad5c8a290b60d79d9acf3f9334d2ba5ee8
devboell <devboell@gmail.com>
2018-07-08 09:16:38 +0200

    filter getCharacter based on searchText

--- src/App/selectors.js
+++ src/App/selectors.js
@@ -3,17 +3,27 @@ import {
   compose,
   toLower,
   prop,
+  filter,
 } from 'ramda'
 import { createSelector } from 'reselect'
 
 const sortByNameCaseInsensitive = sortBy(compose(toLower, prop('name')))
+const filterByName = text => (chr) => {
+  const regex = new RegExp(text, 'i')
+  return chr.name.search(regex) >= 0
+}
 
 const characters = state => state.characters[state.characterType]
+const searchText = state => state.searchText
 
 
 export const getCharacters = createSelector(
   characters,
-  chrs => sortByNameCaseInsensitive(chrs),
+  searchText,
+  (chrs, text) => compose(
+    sortByNameCaseInsensitive,
+    filter(filterByName(text)),
+  )(chrs),
 )
 
 export const getShouldFetch = createSelector(
